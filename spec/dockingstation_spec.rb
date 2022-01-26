a351a45f11036f20eb61e29f6095ed5528d20f17
Haroon <haroon20@hotmail.co.uk>
2022-01-26 16:52:47 +0000

    Building a bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,9 @@
 require_relative "../lib/dockingstation.rb"
 
 describe DockingStation do
-  
+    describe "#release_bike" do
+    docking_station = DockingStation.new # ARRANGE
+      it { is_expected.to respond_to :release_bike }
+    end
 end
+
